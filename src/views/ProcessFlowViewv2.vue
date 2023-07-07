4547ff23016a669dc470712b49e10c539975b735
Jairus Jo T. Casul <caja01@cmi.com.ph>
2023-07-07 13:39:00 +0800

    added the view feature

--- src/views/ProcessFlowViewv2.vue
+++ src/views/ProcessFlowViewv2.vue
@@ -192,6 +192,7 @@
             </div>
             <div class="modal-body">
               <div class="col-md-12 border rounded p-3 mx-auto row">
+                <h6 class="p-3"><em>SETUP PROCESS FLOW MAIN</em></h6>
                 <div class="col-md-1 p-1">
                     <label for="view_flow_no">Flow No.</label>
                     <input id="view_flow_no"  type="text" class="form-control" v-model="viewFlowId" disabled>
@@ -205,7 +206,7 @@
                 </div>
                 <div class="col-md-2 p-1">
                     <label for="view_parts_number">Parts Number</label>
-                    <select @change="getItemCode(mainPartsNumber)" id="view_parts_number"  class="form-select" v-model="viewPartsNumber" disabled>
+                    <select id="view_parts_number"  class="form-select" v-model="viewPartsNumber" disabled>
                         <option v-for="item in itemMaster" :value="item.item_parts_number">{{ item.item_parts_number }}</option>
                     </select>
                 </div>
@@ -227,7 +228,7 @@
                 </div>
                 <div class="col-md-2 p-1">
                     <label for="view_section">Section</label>
-                    <select @change="fetchKeyProcess" v-model="viewSection" class="form-select" id="view_section" disabled>
+                    <select v-model="viewSection" class="form-select" id="view_section" disabled>
                         <option v-for="sec in section" :value="sec.section_id">{{sec.section_code}}</option>
                     </select>
                 </div>
@@ -259,12 +260,12 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="(key, index) in keyProcessFlow">
-                                <td>{{index + 1}}</td>
-                                <td>{{ mainSectionCode }}</td>
+                            <tr v-for="key in sortedKey">
+                                <td>{{ key.sequence_number }}</td>
+                                <td>{{ viewSectionCode }}</td>
                                 <td>
-                                    <select @change="fetchSubProcess" class="form-select" v-model="key.Pid">
-                                        <option v-for="key in keyProcess" :value="key.Pid">{{key.Pname}}</option>
+                                    <select class="form-select" v-model="key.Pid">
+                                        <option v-for="tempKey in tempKeyProcess" :value="tempKey.Pid">{{tempKey.Pname}}</option>
                                     </select>
                                 </td>
                                 <td>
@@ -274,9 +275,7 @@
                                     <input type="text" class="form-control" v-model="key.machine_time">
                                 </td>
                                 <td>
-                                    <button @click="removeKey(index)" class="btn btn-sm btn-outline-danger w-100">
-                                        <span class="material-symbols-outlined">remove</span>
-                                    </button>
+                                    <button class="btn btn-outline-danger w-100">-</button>
                                 </td>
                             </tr>
                         </tbody>
@@ -286,7 +285,7 @@
                         <tbody>
                             <tr>
                                 <td colspan="6">
-                                    <button ref="addKeyBtn" @click="addKeyProcessFlow" class="btn w-100 btn-outline-primary" disabled>Add Row</button>
+                                    <button class="btn w-100 btn-outline-primary" disabled>Add Row</button>
                                 </td>
                             </tr>
                         </tbody>
@@ -304,15 +303,11 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="(sub, index) in subProcessFlow">
-                                <td>{{index + 1}}</td>
+                            <tr v-for="sub in sortedSub">
+                                <td>{{sub.sequence_number}}</td>
                                 <td>{{sub.SubPname}}</td>
-                                <td>
-                                    <input type="text" class="form-control" v-model="sub.standard_time">
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" v-model="sub.machine_time">
-                                </td>                                
+                                <td><input type="text" class="form-control" v-model="sub.standard_time"></td>
+                                <td><input type="text" class="form-control" v-model="sub.machine_time"></td>
                             </tr>
                         </tbody>
                     </table>
@@ -346,9 +341,12 @@
                 processFlow: [],
                 itemMaster: [],
                 keyProcess: [],
+                tempKeyProcess: [],
                 keyProcessFlow: [],
                 subProcessFlow: [],
                 itemList: [],
+                flowKey: [],
+                flowSub: [],
 
                 // main data
                 mainSection: '',
@@ -430,8 +428,75 @@
                         this.viewRevisionNumber = flow.revision_number;
                         this.viewFlowStatus = flow.flow_status;
                         this.viewRemarks = flow.flow_remarks;
+                        
+                        axios.get('http://localhost/tpcrequesthandlers/requestFlowKey.php', {
+                            method: 'GET',
+                            headers: {
+                                'Content-type': 'application/x-www-form-urlencoded'
+                            },
+                            params: {
+                                flow_main_id: this.viewFlowId 
+                            }
+                        }).then(response => {
+                            this.flowKey = response.data;
+                            
+                        }).catch(error => {
+                            console.log(error);
+                        });
+
+                        axios.get('http://localhost/tpcrequesthandlers/requestFlowSub.php', {
+                            method: 'GET',
+                            headers: {
+                                'Content-type': 'application/x-www-form-urlencoded'
+                            },
+                            params: {
+                                flow_main_id: this.viewFlowId 
+                            }
+                        }).then(response => {
+                            this.flowSub = response.data;
+                            for(const sub of this.flowSub){
+                                axios.get('http://localhost/tpcrequesthandlers/requestSubProcess.php',{
+                                    method: 'GET',
+                                    headers: {
+                                        'Content-type': 'application/x-www-form-urlencoded'
+                                    },
+                                    params: {
+                                        Pid: sub.Pid
+                                    }
+                                }).then(response => {
+                                    for(const subProcess of response.data){
+                                        if(subProcess.SubPid == sub.SubPid){
+                                            Object.assign(sub, {SubPname: subProcess.SubPname});
+                                        }
+                                    }
+                                }).catch(error => {
+                                    console.log(error);
+                                });
+                            }
+                        }).catch(error => {
+                            console.log(error);
+                        });
+
+
+                        axios.get('http://localhost/tpcrequesthandlers/requestKeyProcess.php', {
+                            method: 'GET',
+                            headers: {
+                                'Content-type': 'application/x-www-form-urlencoded'
+                            },
+                            params: {
+                                section_id: this.viewSection
+                            }
+                        }).then(response => {
+                            this.tempKeyProcess = response.data;
+                        }).catch(error => {
+                            console.log(error);
+                        });
+
+                        
+
                     }
                 }
+                
             },
             getItemCode(parts_number){
                 this.itemList = [];
@@ -480,7 +545,6 @@
                     sub.sequence_no = sequence_number;
                     sequence_number++;
                 }
-                console.log(this.subProcessFlow);
             },
             
             fetchKeyProcess(){
@@ -540,6 +604,14 @@
                 this.$refs.addKeyBtn.disabled = false;
             },
             
+        },
+        computed:{
+            sortedKey(){
+                return this.flowKey.sort((a, b) => a.sequence_number - b.sequence_number);
+            },
+            sortedSub(){
+                return this.flowSub.sort((a, b) => a.sequence_number - b.sequence_number);
+            }
         },
         created(){
 
